ca2e63f7d4534aa7c678344de4da84198660f3e1
805941736 <805941736@users.noreply.github.com>
2026-07-05 15:06:25 +0800

    Release version 2.0

---
+++ data/backtest-predictions.js
@@ -0,0 +1,10 @@
+﻿window.SSQ_BACKTEST_PREDICTIONS = {
+  "26067": { "red": [5, 8, 10, 18, 23, 30], "blue": [6], "source": "Engel workbook", "sourceRow": 45, "actualRow": 46 },
+  "26068": { "red": [3, 5, 13, 21, 28, 32], "blue": [7], "source": "Engel workbook", "sourceRow": 51, "actualRow": 52 },
+  "26069": { "red": [6, 14, 15, 18, 20, 33], "blue": [1], "source": "Engel workbook", "sourceRow": 57, "actualRow": 58 },
+  "26070": { "red": [8, 11, 16, 17, 28, 32], "blue": [15], "source": "Engel workbook", "sourceRow": 63, "actualRow": 64 },
+  "26071": { "red": [1, 7, 14, 24, 25, 31], "blue": [12], "source": "Engel workbook", "sourceRow": 69, "actualRow": 70 },
+  "26073": { "red": [6, 12, 15, 16, 23, 24], "blue": [15], "source": "Engel workbook", "sourceRow": 75, "actualRow": 76 },
+  "26074": { "red": [2, 4, 9, 15, 17, 22], "blue": [1], "source": "Engel workbook", "sourceRow": 81, "actualRow": 82 },
+  "26075": { "red": [4, 11, 17, 18, 32, 33], "blue": [8], "source": "Engel workbook", "sourceRow": 87, "actualRow": 88 }
+};
